115e714982e3365824c9fc001a256c21da2e4894
Narayan Iyengar <narayan-iyengar@users.noreply.github.com>
2018-05-09 15:21:07 -0700

    update watcher script

--- watch.py
+++ watch.py
@@ -7,18 +7,50 @@ import urllib2
 import urllib
 import subprocess, shlex
 import time 
-def main():
-    api_key = "LUFRPT16Y1BlcXJWSUZrZ3IyQ1BiMXN2d3hSSDhLcUE9eXJiT0ZUdzkyU3I1ZjNIWDhaYzVCTmJaZGhHeFljUWZYQXlQMGViQ1Z6Yz0="
+import threading
+import time
+
+gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+ip = subprocess.check_output(shlex.split("gcloud --format='value(networkInterfaces[0].accessConfigs[0].natIP)' compute instances list --filter='name=firewall1'")).rstrip()
+api_key = "LUFRPT16Y1BlcXJWSUZrZ3IyQ1BiMXN2d3hSSDhLcUE9eXJiT0ZUdzkyU3I1ZjNIWDhaYzVCTmJaZGhHeFljUWZYQXlQMGViQ1Z6Yz0="
+config.load_kube_config(os.path.join(os.environ["HOME"], '.kube/config'))
+v1 = client.CoreV1Api()
+v1ext = client.ExtensionsV1beta1Api()    
+
+def services():
     FWXMLUpdate = []
     XMLHeader = "<uid-message><version>1.0</version><type>update</type><payload>"
     XMLFooter = "</payload></uid-message>"
     Register = "<register>"
-    gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    ip = subprocess.check_output(shlex.split("gcloud --format='value(networkInterfaces[0].accessConfigs[0].natIP)' compute instances list --filter='name=firewall1'")).rstrip()
-    config.load_kube_config(os.path.join(os.environ["HOME"], '.kube/config'))
-    v1 = client.CoreV1Api()
-    mysvc = watch.Watch().stream(v1.list_pod_for_all_namespaces)
-    for event in mysvc:
+
+    mysvcs = watch.Watch().stream(v1.list_service_for_all_namespaces)
+    for event in mysvcs:
+        if event['object'].spec.type == "LoadBalancer":
+          Register += '<entry ip="' + event['object'].spec.load_balancer_ip + '">'
+        else:
+          Register += '<entry ip="' + event['object'].spec.cluster_ip + '">'
+        Register += "<tag>"
+        for i in event['object'].metadata.labels:
+            Register += "<member>" + event['object'].metadata.labels[i] + "</member>"
+        Register += "</tag>"
+        Register += "</entry>"
+        Register += '</register>'
+        FWXMLUpdate = XMLHeader + Register + XMLFooter
+        url = "https://%s/api/?type=user-id&action=set&key=%s&cmd=%s" % (ip, api_key,urllib.quote(FWXMLUpdate))
+        time.sleep(0.5)
+        try:
+            response = urllib2.urlopen(url,context=gcontext).read()
+        except urllib2.HTTPError, e:
+           print "HTTPError = " + str(e)
+
+
+def pods():
+    FWXMLUpdate = []
+    XMLHeader = "<uid-message><version>1.0</version><type>update</type><payload>"
+    XMLFooter = "</payload></uid-message>"
+    Register = "<register>"
+    mypods = watch.Watch().stream(v1.list_pod_for_all_namespaces)
+    for event in mypods:
         Register += '<entry ip="' + event['object'].status.pod_ip + '">'
         Register += "<tag>"
         for i in event['object'].metadata.labels:
@@ -33,6 +65,14 @@ def main():
             response = urllib2.urlopen(url,context=gcontext).read()
         except urllib2.HTTPError, e:
            print "HTTPError = " + str(e)
-            
+
+
+def main():
+    t1 =  threading.Thread(name='watch_pods',target=pods, args=()).start()
+    t2 =  threading.Thread(name='watch_svcs',target=services, args=()).start()
+    while threading.active_count() > 0:
+        time.sleep(1)
+
+
 if __name__ == '__main__':
     main()
