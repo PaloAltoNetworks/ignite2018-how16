d5bd7474debc3bd9e8a0c16051f699dcf57a7152
Narayan Iyengar <narayan-iyengar@users.noreply.github.com>
2018-05-09 15:33:49 -0700

    Add files via upload

--- watch.py
+++ watch.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import os
-from kubernetes import client, config, watch
+#from kubernetes import client, config, watch
 import ssl
 import urllib2
 import urllib
@@ -13,9 +13,23 @@ import time
 gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 ip = subprocess.check_output(shlex.split("gcloud --format='value(networkInterfaces[0].accessConfigs[0].natIP)' compute instances list --filter='name=firewall1'")).rstrip()
 api_key = "LUFRPT16Y1BlcXJWSUZrZ3IyQ1BiMXN2d3hSSDhLcUE9eXJiT0ZUdzkyU3I1ZjNIWDhaYzVCTmJaZGhHeFljUWZYQXlQMGViQ1Z6Yz0="
-config.load_kube_config(os.path.join(os.environ["HOME"], '.kube/config'))
-v1 = client.CoreV1Api()
-v1ext = client.ExtensionsV1beta1Api()    
+kubernetes.config.load_kube_config(os.path.join(os.environ["HOME"], '.kube/config'))
+v1 = kubernetes.client.CoreV1Api()
+v1ext = kubernetes.client.ExtensionsV1beta1Api()    
+
+
+def install_and_import(package):
+    import importlib
+    try:
+        importlib.import_module(package)
+    except ImportError:
+        import pip
+        pip.main(['install', package])
+    finally:
+        globals()[package] = importlib.import_module(package)
+
+
+
 
 def services():
     FWXMLUpdate = []
@@ -23,7 +37,7 @@ def services():
     XMLFooter = "</payload></uid-message>"
     Register = "<register>"
 
-    mysvcs = watch.Watch().stream(v1.list_service_for_all_namespaces)
+    mysvcs = kubernetes.watch.Watch().stream(v1.list_service_for_all_namespaces)
     for event in mysvcs:
         if event['object'].spec.type == "LoadBalancer":
           Register += '<entry ip="' + event['object'].spec.load_balancer_ip + '">'
@@ -49,7 +63,7 @@ def pods():
     XMLHeader = "<uid-message><version>1.0</version><type>update</type><payload>"
     XMLFooter = "</payload></uid-message>"
     Register = "<register>"
-    mypods = watch.Watch().stream(v1.list_pod_for_all_namespaces)
+    mypods = kubernetes.watch.Watch().stream(v1.list_pod_for_all_namespaces)
     for event in mypods:
         Register += '<entry ip="' + event['object'].status.pod_ip + '">'
         Register += "<tag>"
@@ -68,6 +82,7 @@ def pods():
 
 
 def main():
+    install_and_import('kubernetes')
     t1 =  threading.Thread(name='watch_pods',target=pods, args=()).start()
     t2 =  threading.Thread(name='watch_svcs',target=services, args=()).start()
     while threading.active_count() > 0:
